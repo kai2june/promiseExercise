625854a79d6b0ab82643d14e0ead666361f2504f
kai2june <104703027@nccu.edu.tw>
2018-05-17 15:05:30 +0800

    Terminate when first catch is called so that 'unhandled promise warning does not occur.'

--- cleanRoom.js
+++ cleanRoom.js
@@ -1,45 +1,63 @@
 let cleanRoom = function() {
+
     return new Promise( (resolve, reject) => {
-        let isClean = true;
+        let isClean = false;
         if(isClean)
             resolve('Clean');
         else
             reject('Not Clean');
-    });
+        });
 };
-let removeGarbage = new Promise( (resolve, reject) => {
-    let isRemoved = false;
-    if(isRemoved)
-        resolve('Removed');
-    else
-        reject('Not Removed');
-});
+let removeGarbage = function(){
 
-cleanRoom().then( (fromClean) => {
-    console.log(`0 ${fromClean}`);
-    return removeGarbage;
-}).catch( (fromClean) => {
-    console.log(`1 ${fromClean}`);
-    return removeGarbage;
-}).then( (fromRemoved) => {
-    console.log(`2 ${fromRemoved}`); 
-}).catch( (fromRemoved) => {
-    console.log(`3 ${fromRemoved}`);
-});
+    return new Promise( (resolve, reject) => {
+        let isRemoved = false;
+        if(isRemoved)
+            resolve('Removed');
+        else
+            reject('Not removed');
+        });
+};
 
-
-let write = function(){
-    return false;
+let sop = function (){
+    return cleanRoom().then((fromClean)=>{
+        console.log(`10 ${fromClean}`);
+        return removeGarbage();
+    }).then((fromRemove) =>{
+        console.log(`11 ${fromRemove}`);
+    });
 }
-let abc = (async function writeHW(){
-    let isWrote = await write();
-    console.log(isWrote);
-    return new Promise((resolve, reject) => {
-        resolve('Its true');
-    })
-}());
-abc;
-console.log(abc);
-abc.then( (rlt) => {
-    console.log(rlt);
+
+sop().catch( (rlt) => {
+    console.log(`12 ${rlt}`);
+}).catch( (rlt2) => {
+    console.log(`13 ${rlt2}`);
 });
+// cleanRoom(true).then( (fromClean) => {
+//     console.log(`0 ${fromClean}`);
+//     return removeGarbage;
+// }).catch( (fromClean) => {
+//     console.log(`1 ${fromClean}`);
+//     return removeGarbage;
+// }).then( (fromRemoved) => {
+//     console.log(`2 ${fromRemoved}`); 
+// }).catch( (fromRemoved) => {
+//     console.log(`3 ${fromRemoved}`);
+// });
+
+
+// let write = function(){
+//     return false;
+// }
+// let abc = (async function writeHW(){
+//     let isWrote = await write();
+//     console.log(isWrote);
+//     return new Promise((resolve, reject) => {
+//         resolve('Its true');
+//     })
+// }());
+// abc;
+// console.log(abc);
+// abc.then( (rlt) => {
+//     console.log(rlt);
+// });
